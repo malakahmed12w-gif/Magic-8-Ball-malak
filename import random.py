a6feb5c4dbf137fcb17d033599ce8533881b7935
malakahmed12w-gif <malakahmed12w@gmail.com>
2025-09-06 18:04:37 +0200

    implemented random response generation

---
+++ import random.py
@@ -0,0 +1,11 @@
+import random
+responses = [
+     "yes,definitely",
+     "no,not now",
+     "ask again later",
+     "it is certain",
+     "very doubtful",
+     "outlook is good",
+     "better not tell you now",
+     "concentrate and ask again"
+]
